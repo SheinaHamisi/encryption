27c8eb835d12b5a9d0b6c872cb7479ba681fc47d
SheinaHamisi <hamisisheina@gmail.com>
2022-02-21 14:09:16 +0300

    added test to check if it encrypts upper case letters.

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -20,4 +20,16 @@ class EncryptTest {
         Encrypt newEncryption = new Encrypt("z", 1);
         assertEquals("a", newEncryption.forward());
     }
+
+    @Test
+    public void Encrypt_EncryptsWordsLowerCaseCharacters() {
+        Encrypt newEncryption = new Encrypt("abc", 1);
+        assertEquals("bcd", newEncryption.forward());
+    }
+
+    @Test
+    public void Encrypt_EncryptsUpperCaseWords() {
+        Encrypt newEncryption = new Encrypt("ABC", 1);
+        assertEquals("BCD", newEncryption.forward());
+    }
 }
