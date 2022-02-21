b2cad130d45ae92a20b1c1c8c5fdf352faa39fc2
SheinaHamisi <hamisisheina@gmail.com>
2022-02-21 14:31:40 +0300

    added test to decrypt multiple character message.

--- src/test/java/DecryptTest.java
+++ src/test/java/DecryptTest.java
@@ -8,4 +8,10 @@ class DecryptTest {
         Decrypt newDecryption = new Decrypt("z", 1);
         assertEquals("y", newDecryption.backward());
     }
+
+    @Test
+    public void decryptsSmallCaseMultipleCharacterWords() {
+        Decrypt newDecryption = new Decrypt("bcd", 1);
+        assertEquals("abc", newDecryption.backward());
+    }
 }
