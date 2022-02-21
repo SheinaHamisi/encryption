8ddabf36505d4eb9523d1ef64d3cfd2e2eb4060f
SheinaHamisi <hamisisheina@gmail.com>
2022-02-21 13:34:19 +0300

    added test to check if input is a string.

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,3 +1,36 @@
 public class Encrypt {
 
+    private String message;
+    private int key ;
+
+    public Encrypt(String message, int key) {
+        this.message = message;
+        this.key = key;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public boolean isString() {
+        for (int i = 0; i < this.message.length(); i++){
+            if (this.message.charAt(i) >= '0' && this.message.charAt(i) <= '9'){
+                System.out.println("Expected characters or words");
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -1,5 +1,13 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class EncryptTest {
+    @Test
+    public void checkInput_checksIfInputIsString() {
+        Encrypt value = new Encrypt("z", 25);
+        assertEquals(true, value.isString());
+    }
+
 
 }
